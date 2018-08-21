aee897e749cb314ad16b661df4e0047362a2fabd
aaronnorrish <32919148+aaronnorrish@users.noreply.github.com>
2018-08-21 13:02:55 +0800

    add comments to journal_frequency.py

--- journal_frequencies/journal_frequency.py
+++ journal_frequencies/journal_frequency.py
@@ -27,7 +27,9 @@ def scrape_frequencies(url):
         if(page != 1): # prevents requesting the first page again
             source = requests.get(url)
             soup = BeautifulSoup(source.content, 'lxml')
+        # find all instances of journal publishing frequency and ISSN containers
         frequency_and_ISSN = re.findall(r'</dt>\n(.+?)<', str(soup.dl))
+        # find the journal publishing frequency and ISSN
         for journal in frequency_and_ISSN:
             info = journal.lstrip(" ").split(" ")
             # print(info)
@@ -42,6 +44,7 @@ def scrape_frequencies(url):
                 # could just not add to lists, depends whether this is useful information - probably not
         # print(len(frequencies), len(ISSNs)) # for testing
 
+        # find the journal name
         for dt_tag in soup.find_all('dt'):
             journals.append(dt_tag.string[dt_tag.string.find(" ")+1:])
         # print(len(journals))
@@ -56,20 +59,36 @@ def scrape_frequencies(url):
 
     # make dictionary out of the three lists with key = ISSN, value = (journal_name, frequency)
     journals_dictionary = dict(zip(ISSNs, zip(journals, frequencies)))
+
     # print(journals_dictionary)
-    # print(len(journals_dictionary)) # for some reason this is missing one entry for SSCI
+    # print(len(journals_dictionary)) # for some reason this is missing one entry for SSCI, duplicate ISSN??
     return journals_dictionary
 
+# Reads all the journals from the .xlsx SSCI or A&HCI master list and matches
+# them to the journals scraped from the respective master list webpage in order
+# to obtain their publishing frequency. The results are written to another .xlsx
+# file. Note that if a journal from the .xlsx list does not match a journal from
+# the website, or the journal does not have a frequency listed on the website,
+# then the journal is assigned a frequency value of "Not Listed".
+#   @param journals_dictionary a dictionary containing all the journals from
+#       the master list website where key = ISSN, value = (journal_name,
+#       frequency)
+#   @param read_file the SSCI or A&HCI .xlsx master list to be read from
+#   @param write_file the file to which the master list along with journal
+#       publishing frequencies should be written to
 def write_frequencies(journals_dictionary, read_file, write_file):
-    # read xlsx file into lists so the publishing frequencies can be added and wrote
-    # to another xlsx file
+    # read xlsx file into a list so the publishing frequencies can be added and
+    # later written to another xlsx file
     sheet = pd.read_excel(read_file, header=None)
     l = pd.Series.tolist(sheet)
     headers = l.pop(0)
     headers.append("Frequency")
 
     # n = 0
+
+    # loop through each journal from the master list
     for row in l:
+        # attempt to match the ISSN to one from the master list website
         if row[2] in journals_dictionary:
             # n += 1
             if journals_dictionary[row[2]][1] != "Not Listed":
@@ -80,13 +99,35 @@ def write_frequencies(journals_dictionary, read_file, write_file):
         else:
             row.append("Not Listed")
             # print(row[2] in missing)
+
+    # create a pandas dataframe from the existing excel file with the journal
+    # publishing frequencies added to it
     df = pd.DataFrame.from_records(l, columns=headers)
+
     # print(df)
+
+    # write dataframe to excel file
     df.to_excel(write_file, index=False)
+
     # print(n) #3201/3250 by ISSN, string
     # print(len(l))
     # print(l)
 
+# This program may be run with or without command line arguments. If run without
+#  command line arguments, the SSCI and A&HCI Excel master lists must be stored
+#  in the current directory and named publist_ssci.xlsx and publist_ahci.xlsx
+#  respectively. Note this method assumes that the user wants the new data to
+#  written to new Excel files (ssci_frequencies.xlsx and ahci_frequencies.xlsx).
+#  If run with command line arguments, then 6 pieces of information are required,
+#  in the following order:
+#   1. URL to the first webpage of the SSCI master list,
+#   2. the SSCI Excel master list,
+#   3. the Excel file for the updated SSCI information to be written to,
+#   4. URL to the first webpage of the A&HCI master list,
+#   5. the A&HCI Excel master list, and
+#   6. the Excel file for the updated A&HCI information to be written to.
+#  If the number of command line arguments provided is not 0 or 6, then a message
+#  detailing the proper usage of the program will be printed to the command line.
 if __name__ == '__main__':
     if(len(argv) == 1):
         ssci_journals = scrape_frequencies("http://mjl.clarivate.com/cgi-bin/jrnlst/jlresults.cgi?PC=SS&mode=print&Page=1")
