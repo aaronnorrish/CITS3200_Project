239e1c2378c95cf6ce35964fff4eb3a26b803dfa
Aaron Norrish <21972015@student.uwa.edu.au>
2018-10-01 15:18:35 +0800

    update combined springer journal homepage web scraper
    move the separate springer and springer link web scraping code into separate functions

--- submission_URLs/springer/combined_springer_journal_homepage_url.py
+++ submission_URLs/springer/combined_springer_journal_homepage_url.py
@@ -6,30 +6,21 @@ import unicodedata
 # TODO need to remove print statements
 # enclose getting source in try/except
 # try/except for result-items?
+# have functions for springer and springer link ?
+# add a timeout? retry
 
 def remove_accents(string):
     nfkd_form = unicodedata.normalize('NFKD', string)
     return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
-sheet = pd.read_excel("springer_no_duplicates.xlsx", header=None)
-l = pd.Series.tolist(sheet)
-headers = l.pop(0)
+def get_springer_homepage_url(journal_name, ISSN, EISSN):
+    springer_home_url = "https://springer.com"
+    springer_search_url_start = "https://www.springer.com/gp/search?query="
+    springer_search_url_end = "&submit=Submit"
 
-springer_home_url = "https://springer.com"
-springer_search_url_start = "https://www.springer.com/gp/search?query="
-springer_search_url_end = "&submit=Submit"
-
-springer_link_home_url = "https://link.springer.com"
-springer_link_search_url_start = "https://link.springer.com/search?query="
-springer_link_search_url_end = "&facet-content-type=%22Journal%22"
-
-s = 0
-for row in l:
-    # have functions for springer and springer link ?
-    found = False
-    # search_term = re.sub('[^ a-zA-Z0-9]', '', row[0])
-    search_term = row[0].replace("&", "and")
-    # search_term = row[0].replace("&", "%26")
+    # search_term = re.sub('[^ a-zA-Z0-9]', '', journal_name)
+    search_term = journal_name.replace("&", "and")
+    # search_term = journal_name.replace("&", "%26")
     # print(search_term)
     springer_search_url = springer_search_url_start + search_term.replace(" ","+") + springer_search_url_end
     # print(search_url)
@@ -37,66 +28,97 @@ for row in l:
     soup = BeautifulSoup(source.content, 'lxml')
     for div_tag in soup.find_all("div", {"class":"result-item"}):
         if div_tag.small.text == "Journal":
-            journal_url = springer_home_url + div_tag.a.get('href')
-            journal_source = requests.get(journal_url)
+            journal_homepage_relative_path = div_tag.a.get('href')
+            journal_source = requests.get(springer_home_url + journal_homepage_relative_path)
             journal_soup = BeautifulSoup(journal_source.content, 'lxml')
             try:
-                ISSN = journal_soup.find("span", {"wicketpath":"content_basic_productDescriptionContainer_productDescription_issnPrint_content"}).text
-                if(ISSN == row[2]):
-                    # print(row[0])
-                    row.append(journal_url)
-                    s+=1
-                    found = True
-                    break
+                website_ISSN = journal_soup.find("span", {"wicketpath":"content_basic_productDescriptionContainer_productDescription_issnPrint_content"}).text
+                if(website_ISSN == ISSN):
+                    return journal_homepage_relative_path
+                    # print(journal_name)
+                    # row.append(journal_homepage_relative_path)
+                    # s+=1
+                    # break
             except AttributeError:
                 try:
-                    ISSN = journal_soup.find("span", {"wicketpath":"content_basic_productDescriptionContainer_productDescription_issnElectronic_content"}).text
-                    if(ISSN == row[3]):
-                        # print(row[0])
-                        row.append(journal_url)
-                        s+=1
-                        found = True
-                        break
+                    website_EISSN = journal_soup.find("span", {"wicketpath":"content_basic_productDescriptionContainer_productDescription_issnElectronic_content"}).text
+                    if(website_EISSN == ISSN):
+                        return journal_homepage_relative_path
+                        # print(journal_name)
+                        # row.append(journal_homepage_relative_path)
+                        # s+=1
+                        # break
                 except AttributeError:
                     continue
-                    # print("ERROR: unable to get ISSN for ", row[0])
-    if not found:
-        # print("could not find: ", row[0])
-        springer_link_search_url = springer_link_search_url_start + row[2] + springer_link_search_url_end
-        source = requests.get(springer_link_search_url)
-        soup = BeautifulSoup(source.content, 'lxml')
+                    # print("ERROR: unable to get ISSN for ", journal_name)
+    return None
 
-        journals = soup.findAll("li", {"class":"has-cover"})
-        if len(journals) == 0:
-            print("no seach results: ", row[0], row[2])
-        for journal in journals:
-            # for link in :
-            a_tag = journal.find("div", {"class":"text"}).find('a') # assumes each entry has text div
-            journal_path = a_tag.get('href')
-            journal_name = a_tag.text.replace(",", "").lower()
-            if row[0].lower().find(journal_name) != -1 or journal_name.find(row[0].lower()) != -1:
-                row.append(springer_home_url + journal_path)
-                s += 1
-                break
-            else: # the search is done on the ISSN so could just do it anyway
-                # try:
-                # need to open up link to check this
-                # print_issn = issn_tag.find("span", {"class":"pissn"}).split(" ")[0]
-                j = remove_accents(row[0].lower().replace("the ", "").replace(",","").replace("&", "and"))
-                j = re.sub('[^A-Za-z0-9]+', '', j)
-                journal_name = remove_accents(journal_name.lower().replace("the ", "").replace("&", "and"))
-                journal_name = re.sub('[^A-Za-z0-9]+', '', journal_name)
-                if(j == journal_name or j.find(journal_name) != -1 or journal_name.find(j) != -1):
-                    row.append(springer_home_url + journal_path)
-                    s+=1
-                    break
-                else:
-                    print("unable to match: ", soup.find("div", {"id":"issn"}), row[0], row[2])
+def get_springer_link_homepage_url(journal_name, ISSN):
+    springer_link_search_url_start = "https://link.springer.com/search?query="
+    springer_link_search_url_end = "&facet-content-type=%22Journal%22"
+
+    springer_link_search_url = springer_link_search_url_start + ISSN + springer_link_search_url_end
+    source = requests.get(springer_link_search_url)
+    soup = BeautifulSoup(source.content, 'lxml')
+
+    journals = soup.findAll("li", {"class":"has-cover"})
+    if len(journals) == 0:
+        print("no seach results: ", journal_name, ISSN)
+    for journal in journals:
+        # for link in :
+        a_tag = journal.find("div", {"class":"text"}).find('a') # assumes each entry has text div
+        journal_homepage_relative_path = a_tag.get('href')
+        website_journal_name = a_tag.text.replace(",", "").lower()
+        if journal_name.lower().find(website_journal_name) != -1 or website_journal_name.find(journal_name.lower()) != -1:
+            # row.append(springer_home_url + journal_homepage_relative_path)
+            return journal_homepage_relative_path
+            # s += 1
+            # break
+        else: # the search is done on the ISSN so could just do it anyway
+            # try:
+            # need to open up link to check this
+            # print_issn = issn_tag.find("span", {"class":"pissn"}).split(" ")[0]
+            j = remove_accents(journal_name.lower().replace("the ", "").replace(",","").replace("&", "and"))
+            j = re.sub('[^A-Za-z0-9]+', '', j)
+            website_journal_name = remove_accents(website_journal_name.lower().replace("the ", "").replace("&", "and"))
+            website_journal_name = re.sub('[^A-Za-z0-9]+', '', website_journal_name)
+            if(j == website_journal_name or j.find(website_journal_name) != -1 or website_journal_name.find(j) != -1):
+                return journal_homepage_relative_path
+                # row.append(springer_home_url + journal_homepage_relative_path)
+                # s+=1
+                # break
+            else:
+                print("unable to match: ", soup.find("div", {"id":"issn"}), journal_name, ISSN)
+    return None
+
+if __name__ == '__main__':
+    sheet = pd.read_excel("springer_no_duplicates.xlsx", header=None)
+    l = pd.Series.tolist(sheet)
+    headers = l.pop(0)
+
+    springer_home_url = "https://springer.com"
+    springer_link_home_url = "https://link.springer.com"
 
 
-# had a timeout at scientometrics journal
-print(s, len(l))
+    f = 0
+    for row in l:
+        journal_homepage_relative_path = get_springer_homepage_url(row[0], row[2], row[3])
+        if journal_homepage_relative_path is None:
+            print("unable to get from springer:", row[0], row[2])
+            journal_homepage_relative_path = get_springer_link_homepage_url(row[0], row[2])
+        if journal_homepage_relative_path is None:
+            f += 1
+            print("unable to get:", row[0], row[2])
+        row.append(springer_home_url + journal_homepage_relative_path)
+        # if journal_homepage_relative_path is not None:
+        #     instructions_for_authors_URL = get_instructions_for_authors(springer_home_url + journal_homepage_relative_path)
+        #     if instructions_for_authors_URL is not None:
+        #         row.append(instructions_for_authors_URL)
+        #     else:
+        #         row.append(springer_home_url + journal_homepage_relative_path)
 
-headers.append("URL")
-df = pd.DataFrame.from_records(l, columns=headers)
-df.to_excel("combined_springer_journal_homepage_urls.xlsx", index=False)
+    print(len(l) - f, len(l))
+
+    headers.append("URL")
+    df = pd.DataFrame.from_records(l, columns=headers)
+    df.to_excel("combined_springer_journal_homepage_urls.xlsx", index=False)
