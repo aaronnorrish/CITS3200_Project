2638408b439b6145c6aa0c4b411c882228623bea
aaronnorrish <32919148+aaronnorrish@users.noreply.github.com>
2018-10-12 15:57:57 +0800

    update webscraper files

--- submission_URLs/taylor_and_francis/taylor_and_francis.py
+++ submission_URLs/taylor_and_francis/taylor_and_francis.py
@@ -3,150 +3,100 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import sys
 import unicodedata
+import urllib3
+
 
 def remove_accents(string):
     nfkd_form = unicodedata.normalize('NFKD', string)
     return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
-sheet = pd.read_excel("taylor_and_francis_no_duplicates.xlsx", header=None)
-l = pd.Series.tolist(sheet)
-headers = l.pop(0)
 
+def get_taylor_and_francis_homepage(journal, search_term, time_limit):
+    home_url = "https://www.tandfonline.com"
+    search_url_start = "https://www.tandfonline.com/action/doSearch?AllField="
 
-home_url = "https://www.tandfonline.com"
-search_url_start = "https://www.tandfonline.com/action/doSearch?AllField="
-instructions_url_start = "/action/authorSubmission?journalCode="
-instructions_url_end = "&page=instructions"
-# url = "https://www.tandfonline.com/action/doSearch?AllField=1747-7603"
-
-s,t,w, = 0,0,0
-for i in range(len(l)):
+    search_url = search_url_start + search_term
+    source = requests.get(search_url, timeout=time_limit)
+    soup = BeautifulSoup(source.content, 'lxml')
+    found = False
     try:
-        search_url = search_url_start + l[i][2]
-        source = requests.get(search_url)
-        soup = BeautifulSoup(source.content, 'lxml')
-        found = False
-        try:
-            ol_tag = soup.find("ol", {"class":"search-results"})
-            for li_tag in ol_tag.find_all("li"):
-                relative_path = li_tag.find("div", {"class":"publication-meta"}).span.a.get('href')
-                journal_url = home_url + relative_path
-                journal_source = requests.get(journal_url)
-                journal_soup = BeautifulSoup(journal_source.content, 'lxml')
-                journal_name = journal_soup.find("div", {"class":"journalMetaTitle page-heading"}).a.span.text
-                if(l[i][0].lower() == journal_name.lower()):
-                    found = True
-                    s += 1
-                    break
+        ol_tag = soup.find("ol", {"class":"search-results"})
+        for li_tag in ol_tag.find_all("li"):
+            relative_path = li_tag.find("div", {"class":"publication-meta"}).span.a.get('href')
+            journal_url = home_url + relative_path
+            journal_source = requests.get(journal_url, timeout=time_limit)
+            journal_soup = BeautifulSoup(journal_source.content, 'lxml')
+            journal_name = journal_soup.find("div", {"class":"journalMetaTitle page-heading"}).a.span.text
+            if(journal.lower() == journal_name.lower()):
+                return relative_path
+            else:
+                j = remove_accents(journal.lower().replace("the ", "").replace(",","").replace("&", "and"))
+                journal_name = remove_accents(journal_name.lower().replace("the ", "").replace(",","").replace("&", "and"))
+                if(j == journal_name or j.find(journal_name) != -1 or journal_name.find(j) != -1):
+                    return relative_path
                 else:
-                    j = remove_accents(l[i][0].lower().replace("the ", "").replace(",","").replace("&", "and"))
-                    journal_name = remove_accents(journal_name.lower().replace("the ", "").replace(",","").replace("&", "and"))
+                    j = re.sub('[^A-Za-z0-9]+', '', j)
+                    journal_name = re.sub('[^A-Za-z0-9]+', '', journal_name)
                     if(j == journal_name or j.find(journal_name) != -1 or journal_name.find(j) != -1):
-                        found = True
-                        # print(l[i][0].lower(), journal_name.lower())
-                        t += 1
-                        break
+                        return relative_path
                     else:
-                        j = re.sub('[^A-Za-z0-9]+', '', j)
-                        journal_name = re.sub('[^A-Za-z0-9]+', '', journal_name)
-                        # j = j.replace("&", "and").replace("-", "").replace(":", "").replace("'", "").replace(" ", "").replace("/", "").replace('-', '')
-                        # journal_name = journal_name.replace("&", "and").replace("-", "").replace(":", "").replace("'", "").replace(" ", "").replace("/", "").replace('-', '')
-                        # j = j.replace("&", "and").strip(["-", ":", "'", " ", "/", '-'])
-                        # journal_name = journal_name.replace("&", "and").strip(["-", ":", "'", " ", "/", '-'])
-                        if(j == journal_name or j.find(journal_name) != -1 or journal_name.find(j) != -1):
-                            found = True
-                            # print(l[i][0].lower(), journal_name.lower())
-                            w += 1
-                            break
-                        else:
-                            print(j, journal_name)
-            if found:
-                code = re.search('/toc/(.+?)/current', relative_path).group(1)
-                #try: testing if the submission page exists - need a try here
-                # if so l[i].append(instructions_url)
-                # else l[i].append(journal_url)
-                instructions_url = home_url + instructions_url_start + code + instructions_url_end
-                instructions_source = requests.get(instructions_url)
-                instructions_soup = BeautifulSoup(instructions_source.content, 'lxml')
-                if instructions_soup.head.title.text != "Error":
-                    l[i].append(instructions_url)
-                    print("success")
-                else:
-                    print("failure:", l[i][0])
-                    l[i].append(journal_url)
-                print(relative_path, code)
-        except ConnectionResetError:
-            i-=1
-        except AttributeError:
+                        continue
+    except AttributeError:
+        return None
+    return None
+
+def get_taylor_and_francis_instructions_for_authors(relative_path, time_limit):
+    home_url = "https://www.tandfonline.com"
+    instructions_url_start = "/action/authorSubmission?journalCode="
+    instructions_url_end = "&page=instructions"
+
+    code = re.search('/toc/(.+?)/current', relative_path).group(1)
+    instructions_url = home_url + instructions_url_start + code + instructions_url_end
+    instructions_source = requests.get(instructions_url, timeout=time_limit)
+    instructions_soup = BeautifulSoup(instructions_source.content, 'lxml')
+    if instructions_soup.head.title.text != "Error":
+        return instructions_url
+    else:
+        return home_url + relative_path
+
+if __name__ == '__main__':
+    sheet = pd.read_excel("taylor_and_francis_no_duplicates.xlsx", header=None)
+    l = pd.Series.tolist(sheet)
+    headers = l.pop(0)
+
+    s,t,w, = 0,0,0
+    for i in range(len(l)):
+        t +=1
+        exception = True
+        n_tries = 0
+        while(exception and n_tries < 5):
             try:
-                print(l[i][0], l[i][2])
-                search_url = search_url_start + l[i][3]
-                source = requests.get(search_url)
-                soup = BeautifulSoup(source.content, 'lxml')
-                found = False
-                ol_tag = soup.find("ol", {"class":"search-results"})
-                for li_tag in ol_tag.find_all("li"):
-                    relative_path = li_tag.find("div", {"class":"publication-meta"}).span.a.get('href')
-                    journal_url = home_url + relative_path
-                    journal_source = requests.get(journal_url)
-                    journal_soup = BeautifulSoup(journal_source.content, 'lxml')
-                    journal_name = journal_soup.find("div", {"class":"journalMetaTitle page-heading"}).a.span.text
-                    if(l[i][0].lower() == journal_name.lower()):
-                        found = True
-                        s += 1
-                        break
-                    else:
-                        j = remove_accents(l[i][0].lower().replace("the ", "").replace(",","").replace("&", "and"))
-                        journal_name = remove_accents(journal_name.lower().replace("the ", "").replace(",","").replace("&", "and"))
-                        if(j == journal_name or j.find(journal_name) != -1 or journal_name.find(j) != -1):
-                            found = True
-                            # print(l[i][0].lower(), journal_name.lower())
-                            t += 1
-                            break
-                        else:
-                            j = re.sub('[^A-Za-z0-9]+', '', j)
-                            journal_name = re.sub('[^A-Za-z0-9]+', '', journal_name)
-                            # j = j.replace("&", "and").replace("-", "").replace(":", "").replace("'", "").replace(" ", "").replace("/", "").replace('-', '')
-                            # journal_name = journal_name.replace("&", "and").replace("-", "").replace(":", "").replace("'", "").replace(" ", "").replace("/", "").replace('-', '')
-                            # j = j.replace("&", "and").strip(["-", ":", "'", " ", "/", '-'])
-                            # journal_name = journal_name.replace("&", "and").strip(["-", ":", "'", " ", "/", '-'])
-                            if(j == journal_name or j.find(journal_name) != -1 or journal_name.find(j) != -1):
-                                found = True
-                                # print(l[i][0].lower(), journal_name.lower())
-                                w += 1
-                                break
-                            else:
-                                print(j, journal_name)
-                if found:
-                    code = re.search('/toc/(.+?)/current', relative_path).group(1)
-                    #try: testing if the submission page exists - need a try here
-                    # if so l[i].append(instructions_url)
-                    # else l[i].append(journal_url)
-                    instructions_url = home_url + instructions_url_start + code + instructions_url_end
-                    instructions_source = requests.get(instructions_url)
-                    instructions_soup = BeautifulSoup(instructions_source.content, 'lxml')
-                    if instructions_soup.head.title.text != "Error":
-                        l[i].append(instructions_url)
-                        print("success")
-                    else:
-                        l[i].append(journal_url)
-                        print("failure:", l[i][0])
-                    print(relative_path, code)
-            except:
-                print(l[i][0], l[i][2])
-        except:
-            print("exception: ", sys.exc_info())
-    except KeyboardInterrupt:
-        sys.exit()
-    except:
-        print("error ", sys.exc_info())
-        i-=1 # not working
+                journal_homepage_relative_path = get_taylor_and_francis_homepage(l[i][0], l[i][2], 12.0 + n_tries * 12.0)
+                if journal_homepage_relative_path is None:
+                    journal_homepage_relative_path = get_taylor_and_francis_homepage(l[i][0], l[i][3], 12.0 + n_tries * 12.0) # maybe separate instead of nest -> could cause a problem
+                exception = False
+            except(requests.exceptions.RequestException, urllib3.exceptions.HTTPError, urllib3.exceptions.ConnectTimeoutError, urllib3.exceptions.RequestError, urllib3.exceptions.TimeoutError):
+                # pass
+                n_tries += 1
+                exception = True
+        if journal_homepage_relative_path is not None:
+            exception = True
+            n_tries = 0
+            while(exception and n_tries < 5):
+                try:
+                    journal_page = get_taylor_and_francis_instructions_for_authors(journal_homepage_relative_path, 12.0 + n_tries * 12.0)
+                    exception = False
+                except(requests.exceptions.RequestException, urllib3.exceptions.HTTPError, urllib3.exceptions.ConnectTimeoutError, urllib3.exceptions.RequestError, urllib3.exceptions.TimeoutError):
+                    # pass
+                    n_tries += 1
+                    exception = True
+        if journal_page is not None:
+            s+=1
+            print(s, t, s/t)
+            l[i].append(journal_page)
 
-print(s, t, w, len(l), (s+t+w)/len(l))
+    print(s, t, w, len(l), (s+t+w)/len(l))
 
-headers.append("Homepage URL")
-df = pd.DataFrame.from_records(l, columns=headers)
-df.to_excel("taylor_and_francis_journal_homepage_urls.xlsx", index=False)
-
-# if we match journal, extract code then add to submissions url:
-# https://www.tandfonline.com/action/authorSubmission?journalCode=rdsp20&page=instructions
+    headers.append("Homepage URL")
+    df = pd.DataFrame.from_records(l, columns=headers)
+    df.to_excel("taylor_and_francis_journal_homepage_urls.xlsx", index=False)
